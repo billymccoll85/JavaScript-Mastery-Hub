852a06179ce59c2a65d5cb8d142ff6b0ef1b6a0d
CodementorUK <billymccoll2024@gmail.com>
2024-01-16 19:22:20 +0000

    new endpoint added

--- Api-Projects/react/weather-dashboard/src/api/WeatherService.js
+++ Api-Projects/react/weather-dashboard/src/api/WeatherService.js
@@ -15,3 +15,22 @@ export const fetchCurrentWeather = async (city) => {
     throw error;
   }
 };
+
+
+// 5 day forcast 
+export const fetchFiveDayForecast = async (city) => {
+  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  // Using the 5-day forecast endpoint
+  const url = `${API_BASE_URL}/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("5-day forecast data could not be fetched. Please enter a valid city");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching 5-day forecast data:", error);
+    throw error;
+  }
+};
