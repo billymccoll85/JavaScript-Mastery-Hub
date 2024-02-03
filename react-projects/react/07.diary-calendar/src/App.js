05ad20392ad0631a62a462453e79066212e78e30
CodementorUK <billymccoll2024@gmail.com>
2024-02-03 20:23:00 +0000

    Add @vercel/analytics package

--- react-projects/react/07.diary-calendar/src/App.js
+++ react-projects/react/07.diary-calendar/src/App.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import Calendar from './components/Calendar'; // Adjust this import if necessary
-import DiaryEntryModal from './components/DiaryEntryModal'; // Adjust this import if necessary
+import Calendar from './components/Calendar';
+import DiaryEntryModal from './components/DiaryEntryModal';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const App = () => {
   const [selectedDay, setSelectedDay] = useState(null);
   const [entries, setEntries] = useState(() => {
-    // Load saved entries from localStorage
+
     const savedEntries = localStorage.getItem('diaryEntries');
     return savedEntries ? JSON.parse(savedEntries) : {};
   });
 
   useEffect(() => {
-    // Save entries to localStorage whenever they change
+
     localStorage.setItem('diaryEntries', JSON.stringify(entries));
   }, [entries]);
 
   const handleDayClick = (day) => {
-    // Removed the check for future dates
+
     setSelectedDay(day);
   };
 
   const saveEntry = (day, entryText) => {
     const newEntries = { ...entries, [day]: entryText };
     setEntries(newEntries);
-    setSelectedDay(null); // Close the modal after saving
+    setSelectedDay(null); 
   };
 
   return (
@@ -37,6 +39,7 @@ const App = () => {
           currentEntry={entries[selectedDay] || ''}
         />
       )}
+      <Analytics />
     </div>
   );
 };
