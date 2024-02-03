3d0854bdb381a41efd8cef7f7739aeb303d33645
CodementorUK <billymccoll2024@gmail.com>
2024-02-03 19:40:22 +0000

    Update Calendar component styles

--- react-projects/react/07.diary-calendar/src/components/Calendar.jsx
+++ react-projects/react/07.diary-calendar/src/components/Calendar.jsx
@@ -22,21 +22,21 @@ const Calendar = ({ onDayClick, entries }) => {
   };
 
   return (
-    <div className="flex justify-center mt-5">
-      <div className="border border-gray-200 rounded-lg overflow-hidden w-full max-w-screen-lg">
+    <div className="flex justify-center m-4 md:mt-5">
+      <div className="border border-gray-200 rounded-lg overflow-hidden w-full max-w-screen-xl">
         <div className="text-center text-gray-500 bg-gray-100 py-2">{currentMonth} {currentYear}</div>
         <div className="grid grid-cols-1 md:grid-cols-7 w-full">
           {days.map(day => (
             <div
               key={day}
               className={`border border-gray-200 p-2 hover:bg-blue-200 flex flex-col cursor-pointer ${isToday(day) ? 'bg-blue-100' : 'bg-white'}`}
-              style={{ minHeight: '100px' }}
+              style={{ minHeight: '150px' }}
               onClick={() => onDayClick(day)}
             >
-              <div className="text-left text-xs pl-2 pt-1">
+              <div className="text-left text-xs text-gray-500 pl-2 pt-1">
                 <span className="font-semibold">{day}<sup>{getDaySuffix(day)}</sup></span>
               </div>
-              <div className="text-xs text-gray-500 mt-1 overflow-hidden flex-grow">
+              <div className="text-sm text-gray-500 mt-1 overflow-hidden flex-grow">
                 {entries && entries[day] && entries[day].substring(0, 256)}
               </div>
             </div>
