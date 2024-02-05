f6df438f61eff1fd1493d4d73dd070df598ebf55
CodementorUK <billymccoll2024@gmail.com>
2024-02-05 20:04:45 +0000

    Update tailwind.config.js

--- react-projects/react/note-taking-app/tailwind.config.js
+++ react-projects/react/note-taking-app/tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+};
