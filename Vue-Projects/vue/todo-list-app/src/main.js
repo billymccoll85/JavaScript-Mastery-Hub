e48faea4bac7bf62a4f870068b8142e9831a3a17
CodementorUK <billymccoll2024@gmail.com>
2024-02-13 19:14:17 +0000

    Update store import in main.js

--- Vue-Projects/vue/todo-list-app/src/main.js
+++ Vue-Projects/vue/todo-list-app/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './store/modules/todoList'
 import './assets/styles.css';
 
 createApp(App).use(store).mount('#app')
