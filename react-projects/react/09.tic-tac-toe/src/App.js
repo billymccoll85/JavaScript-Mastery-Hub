a94706747fd13b35e8f6578016f5adc1196a15ac
CodementorUK <billymccoll2024@gmail.com>
2024-03-18 14:22:16 +0000

    Refactor Square component and add Board component

--- react-projects/react/09.tic-tac-toe/src/App.js
+++ react-projects/react/09.tic-tac-toe/src/App.js
@@ -1,3 +1,25 @@
-export default function Square() {
-    return <button className="sqaure">X</button>
+function Square({ value }) {
+    return <button className="square">{value}</button>
+}
+
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square value='1' />
+                <Square value='2' />
+                <Square value='3' />
+            </div>
+            <div className="board-row">
+                <Square value='4' />
+                <Square value='5' />
+                <Square value='6' />
+            </div>
+            <div className="board-row">
+                <Square value='7' />
+                <Square value='8' />
+                <Square value='9' />
+            </div>
+        </>
+    )
 }
