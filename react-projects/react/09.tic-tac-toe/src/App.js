f55db929f9aab6be5aea38eb81f78e9b9c869820
CodementorUK <billymccoll2024@gmail.com>
2024-03-18 15:44:38 +0000

    Add winner calculation and display status message

--- react-projects/react/09.tic-tac-toe/src/App.js
+++ react-projects/react/09.tic-tac-toe/src/App.js
@@ -6,17 +6,58 @@ function Square({ value, onSquareClick }) {
     )
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+}
+
 export default function Board() {
+    const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState(Array(9).fill(null))
 
     function handleClick(i) {
-        const nextSquare = squares.slice()
-        nextSquare[i] = 'X'
-        setSquares(nextSquare)
+        if(squares[i]) {
+            return
+        }
+
+        const nextSquares = squares.slice()
+
+        if(xIsNext) {
+            nextSquares[i] = 'X'
+        } else {
+            nextSquares[i] = 'O'
+        }
+        setSquares(nextSquares)
+        setXIsNext(!xIsNext)
+    }
+
+    const winner = calculateWinner(squares)
+    let status
+
+    if(winner) {
+        status = "Winner: " + winner
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O")
     }
 
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
