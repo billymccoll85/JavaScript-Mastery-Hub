5122cdad774c0cfd1a75b24fd44f9c35994bdbb6
CodementorUK <billymccoll2024@gmail.com>
2024-04-18 18:32:13 +0100

    Update ResultCard component styling

--- react/complete-react-guide/05.quiz-app-events/src/components/QuizContainer.jsx
+++ react/complete-react-guide/05.quiz-app-events/src/components/QuizContainer.jsx
@@ -65,88 +65,26 @@ const QuizContainer = () => {
             { text: "call myFunction()", isCorrect: false },
             { text: "call function myFunction()", isCorrect: false },
             { text: "run myFunction()", isCorrect: false }
-        ]},
-        { id: 11, questionText: "Which property is used to change the background color of an element?", answers: [
-            { text: "background-color", isCorrect: true },
-            { text: "bgcolor", isCorrect: false },
-            { text: "color", isCorrect: false },
-            { text: "background", isCorrect: false }
-        ]},
-        { id: 12, questionText: "What does CSS stand for?", answers: [
-            { text: "Cascading Style Sheets", isCorrect: true },
-            { text: "Creative Style Sheets", isCorrect: false },
-            { text: "Computer Style Sheets", isCorrect: false },
-            { text: "Colorful Style Sheets", isCorrect: false }
-        ]},
-        { id: 13, questionText: "How do you select an element with id 'demo' in CSS?", answers: [
-            { text: "#demo", isCorrect: true },
-            { text: ".demo", isCorrect: false },
-            { text: "demo", isCorrect: false },
-            { text: "*demo", isCorrect: false }
-        ]},
-        { id: 14, questionText: "Which HTML tag is used to define an internal style sheet?", answers: [
-            { text: "<style>", isCorrect: true },
-            { text: "<css>", isCorrect: false },
-            { text: "<script>", isCorrect: false },
-            { text: "<link>", isCorrect: false }
-        ]},
-        { id: 15, questionText: "What is the correct CSS syntax for making all the <p> elements bold?", answers: [
-            { text: "p {font-weight:bold;}", isCorrect: true },
-            { text: "p {text:bold;}", isCorrect: false },
-            { text: "p {style:bold;}", isCorrect: false },
-            { text: "p {bold:true;}", isCorrect: false }
-        ]},
-        { id: 16, questionText: "Which event occurs when the user clicks on an HTML element?", answers: [
-            { text: "onchange", isCorrect: false },
-            { text: "onmouseclick", isCorrect: false },
-            { text: "onmouseover", isCorrect: false },
-            { text: "onclick", isCorrect: true }
-        ]},
-        { id: 17, questionText: "How can you add a comment in a JavaScript?", answers: [
-            { text: "//This is a comment", isCorrect: true },
-            { text: "<!--This is a comment-->", isCorrect: false },
-            { text: "'This is a comment", isCorrect: false },
-            { text: "*/This is a comment/*", isCorrect: false }
-        ]},
-        { id: 18, questionText: "Which attribute specifies the URL of the image to use as a background for an element?", answers: [
-            { text: "background", isCorrect: false },
-            { text: "img", isCorrect: false },
-            { text: "src", isCorrect: false },
-            { text: "background-image", isCorrect: true }
-        ]},
-        { id: 19, questionText: "Which HTML attribute specifies the URL of the page the link goes to?", answers: [
-            { text: "href", isCorrect: true },
-            { text: "link", isCorrect: false },
-            { text: "src", isCorrect: false },
-            { text: "url", isCorrect: false }
-        ]},
-        { id: 20, questionText: "How do you declare a JavaScript variable?", answers: [
-            { text: "v myVariable;", isCorrect: false },
-            { text: "variable myVariable;", isCorrect: false },
-            { text: "var myVariable;", isCorrect: true },
-            { text: "myVariable = var;", isCorrect: false }
         ]}
+
     ], []);
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
     const [score, setScore] = useState(0);
+    const [quizFinished, setQuizFinished] = useState(false);
 
     // Calculate the score
     useEffect(() => {
-        const calculateScore = () => {
-            let newScore = userAnswers.reduce((acc, answer) => {
+        if (quizFinished) {
+            const newScore = userAnswers.reduce((acc, answer) => {
                 const question = questions.find(q => q.id === answer.questionId);
                 const correctAnswer = question.answers.find(a => a.isCorrect).text;
                 return acc + (answer.answerText === correctAnswer ? 1 : 0);
             }, 0);
             setScore(newScore);
-        };
-
-        if (userAnswers.length === questions.length) {
-            calculateScore();
         }
-    }, [userAnswers, questions]); // Now questions is stable and won't cause re-runs
+    }, [userAnswers, questions, quizFinished]);
 
     const handleAnswerSelection = (answerText) => {
         const newAnswers = [...userAnswers, { questionId: questions[currentQuestionIndex].id, answerText }];
@@ -156,6 +94,8 @@ const QuizContainer = () => {
         const nextQuestionIndex = currentQuestionIndex + 1;
         if (nextQuestionIndex < questions.length) {
             setCurrentQuestionIndex(nextQuestionIndex);
+        } else {
+            setQuizFinished(true); // Mark the quiz as finished
         }
     };
 
@@ -167,16 +107,15 @@ const QuizContainer = () => {
     };
 
     const goToPrevious = () => {
-        const prevQuestionIndex = currentQuestionIndex - 1;
-        if (prevQuestionIndex >= 0) {
-            setCurrentQuestionIndex(prevQuestionIndex);
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
         }
     };
 
     return (
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 min-h-screen flex flex-col justify-center items-center">
+        <div className="container my-20 mx-auto px-4 sm:px-6 lg:px-8 min-h-screen flex flex-col justify-center items-center">
             <h1 className="text-2xl font-bold text-center mb-6">Quiz on Web Development</h1>
-            {questions && questions.length > 0 && (
+            {!quizFinished ? (
                 <div className="w-full max-w-4xl">
                     <QuestionCard
                         question={questions[currentQuestionIndex]}
@@ -188,25 +127,22 @@ const QuizContainer = () => {
                         currentIndex={currentQuestionIndex}
                         totalQuestions={questions.length}
                     />
-                    
-                    {userAnswers.length === questions.length && (
-                        <div>
-                            {questions.map((question, index) => (
-                                <ResultCard
-                                    key={question.id}
-                                    question={question}
-                                    userAnswer={userAnswers.find(answer => answer.questionId === question.id)?.answerText}
-                                />
-                            ))}
-                            <div className="score mt-4 text-center">
-                                Your score: {score} / {questions.length}
-                            </div>
-                        </div>
-                    )}
-
                     <p className="text-sm mb-2 text-center">
                         Question {currentQuestionIndex + 1} of {questions.length}
                     </p>
+                </div>
+            ) : (
+                <div className="w-full max-w-4xl">
+                    {questions.map((question, index) => (
+                        <ResultCard
+                            key={question.id}
+                            question={question}
+                            userAnswer={userAnswers.find(answer => answer.questionId === question.id)?.answerText}
+                        />
+                    ))}
+                    <div className="score mt-4 text-center">
+                        Your score: {score} / {questions.length}
+                    </div>
                 </div>
             )}
         </div>
--- react/complete-react-guide/05.quiz-app-events/src/components/ResultCard.jsx
+++ react/complete-react-guide/05.quiz-app-events/src/components/ResultCard.jsx
@@ -4,7 +4,7 @@ const ResultCard = ({ question, userAnswer }) => {
     const isCorrect = question.answers.find(ans => ans.text === userAnswer)?.isCorrect;
 
     return (
-        <div className={`p-4 my-2 rounded-lg text-white ${isCorrect ? 'bg-green-500' : 'bg-red-500'}`}>
+        <div className={`p-4 my-4 rounded-lg text-white ${isCorrect ? 'bg-green-500' : 'bg-red-500'}`}>
             <h3 className="font-semibold">{question.questionText}</h3>
             <p>Your answer: {userAnswer}</p>
             <p>{isCorrect ? 'Correct' : 'Incorrect'}</p>
